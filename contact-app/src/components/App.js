b8d35424ceb72ab9fb36af732810f23debee2742
olusegun Owolabi <olujawo1996@gmail.com>
2023-03-23 12:35:37 +0100

    contact card; state and props
    States and props generated

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -5,11 +5,25 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Owolabi",
+      email: "olujawo1996@gmail.com",
+    },
+
+    {
+      id: "2",
+      name: "Segun",
+      email: "oluwajawo@gmail.com",
+    },
+  ];
+
   return (
-    <div>
+    <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
--- contact-app/src/components/ContactList.js
+++ contact-app/src/components/ContactList.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const ContactList = () => {
-  return <div>ContactList</div>;
+const ContactList = (props) => {
+  console.log(props);
+
+  const renderContactList = props.contacts.map((contact) => {
+    return (    );
+  });
+  return <div className="ui celled List">{renderContactList}</div>;
 };
 
 export default ContactList;
