b27ca40ebba265507255139f73c5c5dedee55803
olusegun Owolabi <olujawo1996@gmail.com>
2023-03-28 13:21:23 +0100

    Chapter 1 - 4

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = <h1>Hello, World</h1>;
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world</h1>
+      <h2>It is {nre </h2>
+    </div>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
