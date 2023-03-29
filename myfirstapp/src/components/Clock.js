53dd5280e83f7f89d023e59f409687e6a5bb57d0
olusegun Owolabi <olujawo1996@gmail.com>
2023-03-29 10:30:18 +0100

    component lifecycle - useState, useEffect

--- myfirstapp/src/components/Clock.js
+++ myfirstapp/src/components/Clock.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Clock() {
   const [time, setTime] = useState(new Date().toString());
+  const [flag, setFlag] = useState(true);
+
+  useEffect(() => {
+    console.log("componenet mounted or updated");
+    const interval = setInterval(showDate, 1000);
+
+    return () => {
+      console.log("cleanup of Interval");
+      clearInterval(interval);
+    };
+  }, [time]);
+
   function showDate() {
     setTime(new Date().toString());
   }
+
   setInterval(showDate, 1000);
 
   return <div>{time}</div>;
