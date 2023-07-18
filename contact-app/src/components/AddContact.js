812ca3b9e2b9acff12b81ff2c15b67f97b74ba2f
Owolabi olusegun <52004465+boarnerges@users.noreply.github.com>
2023-07-18 08:16:50 +0100

    coming up with components; App, AddContact, ContactList

---
+++ contact-app/src/components/AddContact.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class AddContact extends Component {
+  render() {
+    return (
+      <div className="ui main">
+        <h2>Add Contact</h2>
+        <form className="ui form">
+          <div className="field">
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name"></input>
+          </div>
+        </form>
+
+        <form className="ui form">
+          <div className="field">
+            <label>Email</label>
+            <input type="text" name="email" placeholder="Email"></input>
+          </div>
+          <button className="ui button blue">Add</button>
+        </form>
+      </div>
+    );
+  }
+}
--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -5,11 +5,25 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "owolabi",
+      email: "owolabi@gmail.com",
+    },
+
+    {
+      id: "2",
+      name: "segun",
+      email: "segun@gmail.com",
+    },
+  ];
+
   return (
-    <div>
+    <div className="ui container">
       <Header />
-      {/* <AddContact />
-      <ContactList /> */}
+      <AddContact />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
