963b7ca69bc6ad4d8deb5ae38fe84878f1d66ca1
Owolabi olusegun <52004465+boarnerges@users.noreply.github.com>
2023-07-18 07:47:55 +0100

    Additonals - semantic ui setup and stuff

---
+++ contact-app/src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <AddContact />
+      <ContactList /> */}
+    </div>
+  );
+}
+
+export default App;
+
+//Component is a piece of reusable code which has its own functionality in it
